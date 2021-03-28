a4c7d8462386549a6e6ba73b8cac76af88a260e9
johnathanPrisCorp <81187162+johnathanPrisCorp@users.noreply.github.com>
2021-03-28 12:15:10 -0500

    Added middleware to check for token in headers. Added auth route logic.

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express()
 
 connectDB()
 
-app.use(express.json());
-app.use(cors({origin:true,credentials: true}));
+app.use(express.json())
+
+app.use(cors())
 
 app.use("/api/users", require("./routes/users"))
+app.use("/api/auth", require("./routes/auth"))
 
 const PORT = process.env.PORT || 5000
 
